f2f7bf383b0c75b96b8ae15046fd8306500b3563
moesama <moesama_zzt@163.com>
2023-08-15 17:31:27 +0800

    feat: init code

--- server-core/src/main/java/com/doporro/core/handler/EntityResultHandler.java
+++ server-core/src/main/java/com/doporro/core/handler/EntityResultHandler.java
@@ -3,13 +3,22 @@ package com.doporro.core.handler;
 import org.springframework.core.ReactiveAdapterRegistry;
 import org.springframework.http.codec.HttpMessageWriter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.HandlerResult;
 import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityResultHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 
 import java.util.List;
 
 //@Component
 public class EntityResultHandler extends ResponseEntityResultHandler {
+
+    @Override
+    public @NonNull Mono<Void> handleResult(@NonNull ServerWebExchange exchange, @NonNull HandlerResult result) {
+        return super.handleResult(exchange, result);
+    }
 
     public EntityResultHandler(
             List<HttpMessageWriter<?>> writers, RequestedContentTypeResolver resolver, ReactiveAdapterRegistry registry
--- server-core/src/main/java/com/doporro/core/handler/ResponseHandler.java
+++ server-core/src/main/java/com/doporro/core/handler/ResponseHandler.java
@@ -1,13 +1,22 @@
 package com.doporro.core.handler;
 
 import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.web.reactive.HandlerResult;
 import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
 import org.springframework.web.reactive.result.method.annotation.ResponseBodyResultHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 
 import java.util.List;
 
 //@Component
 public class ResponseHandler extends ResponseBodyResultHandler {
+
+    @Override
+    public @NonNull Mono<Void> handleResult(@NonNull ServerWebExchange exchange, @NonNull HandlerResult result) {
+        return super.handleResult(exchange, result);
+    }
 
     public ResponseHandler(List<HttpMessageWriter<?>> writers, RequestedContentTypeResolver resolver) {
         super(writers, resolver);
