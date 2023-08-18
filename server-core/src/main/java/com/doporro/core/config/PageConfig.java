1813eb353017c817d905274b514b3f729d7e178f
moesama <moesama_zzt@163.com>
2023-08-18 14:05:52 +0800

    feat: init code

--- server-core/src/main/java/com/doporro/core/config/PageConfig.java
+++ server-core/src/main/java/com/doporro/core/config/PageConfig.java
@@ -1,10 +1,8 @@
 package com.doporro.core.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableCaching
-public class CacheConfig implements WebFluxConfigurer {
+public class PageConfig implements WebFluxConfigurer {
 }
