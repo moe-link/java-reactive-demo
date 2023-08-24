93b2341ac761badeb4aa169506330d45ff267938
moesama <moesama_zzt@163.com>
2023-08-24 14:39:38 +0800

    :rocket: FEAT: init code

--- server-base/src/main/java/com/doporro/base/convert/UserActionConvert.java
+++ server-base/src/main/java/com/doporro/base/convert/UserActionConvert.java
@@ -1,7 +1,12 @@
 package com.doporro.base.convert;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
+@SuppressWarnings(value = "unused")
 public class UserActionConvert {
+
+    UserActionConvert INSTANCE  = Mappers.getMapper(UserActionConvert.class);
+
 }
