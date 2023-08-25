db8b68b4a87e65395beb099d80c3e7ea26d0be6f
moesama <moesama_zzt@163.com>
2023-08-25 16:28:46 +0800

    :rocket: FEAT: init code

---
+++ server-core/src/main/java/com/doporro/core/config/ScheduleConfig.java
@@ -0,0 +1,11 @@
+package com.doporro.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class ScheduleConfig implements WebFluxConfigurer {
+
+
+
+}
