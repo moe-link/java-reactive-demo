3bcc98f5e08d3058c9946f0d2ed4cf87b24d58c8
moesama <moesama_zzt@163.com>
2023-09-19 17:35:25 +0800

    :rocket: FEAT: success code

--- server-core/src/main/java/com/doporro/core/config/SecurityConfig.java
+++ server-core/src/main/java/com/doporro/core/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.doporro.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,8 +14,16 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 public class SecurityConfig implements WebFluxConfigurer {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http.build();
     }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityWebFilterChain authorizationServerSecurityFilterChain(ServerHttpSecurity http) {
+        return http.build();
+    }
+
 
 }
